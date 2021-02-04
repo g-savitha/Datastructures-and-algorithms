1e5ec7809ca15d917a557523414f6259640bbe55
G Savitha <mailto@gsavitha.in>
2021-02-04 16:49:10 +0530

    added code for Array Operations

---
+++ Arrays/ArrayOperations.java
@@ -0,0 +1,37 @@
+public class ArrayOperations{
+    static int insert(int arr[], int n, int x, int capacity, int position)
+    {
+        if(n == capacity) return n;
+        
+        int idx = position - 1;
+        
+    	for(int i = n - 1; i >= idx; i--){
+    		arr[i + 1] = arr[i];
+        }
+        
+    	arr[idx] = x;
+    	return n + 1;
+    } 
+    static int search(int arr[], int n, int x)
+    {
+    	for(int i = 0; i < n; i++){
+    		if(arr[i] == x)
+    			return i;
+    	}
+    	return -1;
+    } 
+    static int delete(int arr[], int n, int x)
+    {
+    	int i = 0;
+    	for(i = 0; i < n; i++) {
+    		if(arr[i] == x)
+    			break;
+    	}
+    	if(i == n) return n;
+
+    	for(int j = i; j < n - 1; j++){
+    		arr[j] = arr[j + 1];
+    	}
+    	return n-1;
+    } 
+}
